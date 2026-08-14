10396d897875b5201ceebbcc5cc417d2da4d40bf
Rafael Paroni <91437378+RafaelParoni@users.noreply.github.com>
2026-08-14 15:46:21 -0300

    Removendo arquivos não usados no sistema web UI

--- run_log.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-with open("error.txt", "w") as f:
-    subprocess.run(["d:/MyPrograms/UniversalMediaDownloader/.venv/Scripts/python.exe", "youtube_downloader.py"], stderr=f, stdout=f)
